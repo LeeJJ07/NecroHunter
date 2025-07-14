9ea0ecfa1f56ff06da0e3cef16e3b925f860566d
LeeJJ07 <jkj9513@naver.com>
2025-07-15 01:20:29 +0900

    feat: 터치 기반 플레이어 이동 기능 임시 구현
    화면 터치 입력을 활용한 기본 이동 기능을 테스트용으로 구현하고, 시작 터치 지점을 기준으로 방향 벡터를 계산하여 캐릭터 이동에 적용

--- NecroHunter/Assets/Scripts/Input/PlayerInputHandler.cs
+++ NecroHunter/Assets/Scripts/Input/PlayerInputHandler.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.EnhancedTouch;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private NavMeshAgent player;
+
+    private Finger movementFinger;
+    private Vector2 movementAmount;
+
+    private Vector2 startFingerPosition;
+    [SerializeField] private float maxMovement = 150.0f;
+
+    private void OnEnable()
     {
-        
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += HandleFingerDown;
+        ETouch.Touch.onFingerUp += HandleLoseFinger;
+        ETouch.Touch.onFingerMove += HandleFingerMove;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        ETouch.Touch.onFingerDown -= HandleFingerDown;
+        ETouch.Touch.onFingerUp -= HandleLoseFinger;
+        ETouch.Touch.onFingerMove -= HandleFingerMove;
+        EnhancedTouchSupport.Disable();
+    }
+
+    private void HandleFingerMove(Finger movedFinger)
+    {
+        if(movedFinger == movementFinger)
+        {
+            Vector2 knobPosition;
+            ETouch.Touch currentTouch = movedFinger.currentTouch;
+
+            if(Vector2.Distance(currentTouch.screenPosition,
+                startFingerPosition) > maxMovement)
+            {
+                knobPosition = (
+                    currentTouch.screenPosition - startFingerPosition
+                    ).normalized * maxMovement;
+            }
+            else
+            {
+                knobPosition = currentTouch.screenPosition - startFingerPosition;
+            }
+
+            movementAmount = knobPosition / maxMovement;
+            Debug.Log(movementAmount);
+        }
+    }
+    private void HandleLoseFinger(Finger lostFinger)
+    {
+        if(lostFinger == movementFinger)
+        {
+            movementFinger = null;
+            movementAmount = Vector2.zero;
+            startFingerPosition = Vector2.zero;
+        }
+    }
+    private void HandleFingerDown(Finger touchedFinger)
+    {
+        if(movementFinger == null)
+        {
+            movementFinger = touchedFinger;
+            movementAmount = Vector2.zero;
+
+            startFingerPosition = touchedFinger.screenPosition;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 scaledMovement = player.speed * Time.deltaTime * new Vector3(
+            movementAmount.x,
+            0.0f,
+            movementAmount.y
+        );
+
+        player.Move(scaledMovement);
+        player.transform.LookAt(player.transform.position + scaledMovement, Vector3.up);
     }
 }
