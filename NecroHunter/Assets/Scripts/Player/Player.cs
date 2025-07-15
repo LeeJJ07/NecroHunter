a83215c25099490f7cc939b7be9a2931c75c95c0
LeeJJ07 <jkj9513@naver.com>
2025-07-15 12:04:55 +0900

    feat: 플레이어 FSM에 Idle 및 Run 상태 추가
    플레이어 클래스에 상태 머신을 기반으로 Idle 상태와 이동 상태를 초기화하고, NavMeshAgent 및 입력 처리 컴포넌트를 연동

--- NecroHunter/Assets/Scripts/Player/Player.cs
+++ NecroHunter/Assets/Scripts/Player/Player.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region State Variables
+    public PlayerStateMachine StateMachine { get; private set; }
+    public PlayerIdleState IdleState { get; private set; }
+    public PlayerMoveState MoveState { get; private set; }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    #region Components
+    public Animator Anim { get; private set; }
+    public PlayerInputHandler InputHandler { get; private set; }
+    public NavMeshAgent Agent { get; private set; }
+    #endregion
+
+    #region Other Variables
+    [SerializeField]
+    private PlayerData playerData;
+    #endregion
+
+    #region Unity Callback Functions
+    private void Awake()
     {
-        
+        StateMachine = new PlayerStateMachine();
+
+        IdleState = new PlayerIdleState(this, StateMachine, playerData, "idle");
+        MoveState = new PlayerMoveState(this, StateMachine, playerData, "move");
     }
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+        InputHandler = GetComponent<PlayerInputHandler>();
+        Agent = GetComponent<NavMeshAgent>();
+
+        StateMachine.Initialize(IdleState);
+    }
+    private void Update()
+    {
+        StateMachine.CurrentState.LogicUpdate();
+    }
+    private void FixedUpdate()
+    {
+        StateMachine.CurrentState.PhysicsUpdate();
+    }
+    #endregion
 }
