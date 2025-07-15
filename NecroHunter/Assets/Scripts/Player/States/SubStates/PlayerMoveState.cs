326394db67895d989e3b9d358d90bbeee8cd499a
LeeJJ07 <jkj9513@naver.com>
2025-07-15 12:00:10 +0900

    feat: 플레이어 Move 상태 구현
    플레이어 StateMachine에 이동 상태 클래스를 추가하고, 입력 벡터에 따라 이동 및 방향 전환을 처리하는 로직을 구현

--- NecroHunter/Assets/Scripts/Player/States/SubStates/PlayerMoveState.cs
+++ NecroHunter/Assets/Scripts/Player/States/SubStates/PlayerMoveState.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveState : MonoBehaviour
+public class PlayerMoveState : PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerMoveState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void DoCheck()
     {
-        
+        base.DoCheck();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        Vector3 scaledMovement = playerData.moveSpeed * Time.deltaTime * new Vector3(
+            movementAmount.x,
+            0.0f,
+            movementAmount.y
+        );
+
+        player.Agent.Move(scaledMovement);
+        player.transform.LookAt(player.transform.position + scaledMovement, Vector3.up);
+
+        if (movementAmount.magnitude == 0.0f)
+        {
+            stateMachine.ChangeState(player.IdleState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
     }
 }
