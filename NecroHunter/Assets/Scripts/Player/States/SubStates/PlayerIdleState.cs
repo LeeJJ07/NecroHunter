bbee1f9951f6f7abbda834bfff0c88a4854c8813
LeeJJ07 <jkj9513@naver.com>
2025-07-15 11:59:28 +0900

    feat: 플레이어 Idle 상태 구현
    플레이어 상태 머신 시스템에 Idle 상태 클래스를 추가하고, 이동 입력 감지 시 Move 상태로 전환하는 로직을 구성

--- NecroHunter/Assets/Scripts/Player/States/SubStates/PlayerIdleState.cs
+++ NecroHunter/Assets/Scripts/Player/States/SubStates/PlayerIdleState.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerIdleState : MonoBehaviour
+public class PlayerIdleState : PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerIdleState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void DoCheck()
     {
-        
+        base.DoCheck();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (movementAmount.magnitude != 0.0f)
+            stateMachine.ChangeState(player.MoveState);
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
     }
 }
