278c2c2f8914619e8e2764090ceaf566b422469f
LeeJJ07 <jkj9513@naver.com>
2025-07-17 22:08:51 +0900

    feat: Hauling 상태 구조 구현

--- NecroHunter/Assets/Scripts/Player/States/SubStates/PlayerHaulingState.cs
+++ NecroHunter/Assets/Scripts/Player/States/SubStates/PlayerHaulingState.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHaulingState : MonoBehaviour
+public class PlayerHaulingState : PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerHaulingState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void DoCheck()
     {
-        
+        base.DoCheck();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
     }
 }
