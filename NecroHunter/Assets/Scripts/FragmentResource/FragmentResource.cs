58ab5d83ba44b77b36d3619c5fb436b0b0c72e17
LeeJJ07 <jkj9513@naver.com>
2025-07-21 02:10:27 +0900

    feat: 파편 자원 수집 구현 및 연출 구현
    로프, 파편자원 풀에 반납

--- NecroHunter/Assets/Scripts/FragmentResource/FragmentResource.cs
+++ NecroHunter/Assets/Scripts/FragmentResource/FragmentResource.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using GogoGaga.OptimizedRopesAndCables;
 using UnityEngine;
-using UnityEngine.AI;
-
 public class FragmentResource : MonoBehaviour
 {
     [SerializeField] private Rope ropePrefab;
@@ -48,6 +45,68 @@ public class FragmentResource : MonoBehaviour
 
         ropePrefab.SetStartPoint(player.GetComponent<Player>().RopePoint);
         ropePrefab.SetEndPoint(transform);
-        rope = ObjectPoolManager.Instantiate(ropePrefab, transform.position, Quaternion.identity);
+
+        rope = ObjectPoolManager.SpawnObject(ropePrefab, transform.position, Quaternion.identity);
+
+        rope.SetStartPoint(player.GetComponent<Player>().RopePoint);
+        rope.SetEndPoint(transform);
+        rope.RecalculateRope();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "ResourceCollectorZone":
+                MoveToCenter(other.transform.position);
+                break;
+
+            case "BaseCamp":
+                GetResource();
+                break;
+
+            default:
+                break;
+        }
+    }
+    private void MoveToCenter(Vector3 center)
+    {
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        float duration = 1.2f;
+        float arcHeight = 1.0f;
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = center;
+
+        Vector3 midPoint = (startPos + endPos) * 0.5f;
+        midPoint.y += arcHeight;
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(transform.DOJump(midPoint, arcHeight, 1, duration * 0.5f).SetEase(Ease.OutQuad));
+        seq.Append(transform.DOMove(endPos, duration * 0.5f).SetEase(Ease.InQuad));
+
+        seq.OnComplete(() =>
+        {
+            rb.useGravity = true;
+        });
+
+        ReturnRope();
+        player.GetComponent<Player>().ReturnHaulResource();
+    }
+    
+    private void GetResource()
+    {
+        player = null;
+        IsLinkedToPlayer = false;
+
+        ObjectPoolManager.ReturnObjectToPool(gameObject);
+    }
+    private void ReturnRope()
+    {
+        ObjectPoolManager.ReturnObjectToPool(rope.gameObject);
     }
 }
